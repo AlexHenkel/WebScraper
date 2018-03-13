373f6969a03d11991285eef18eeeeaea5beb1b78
Alejandro Henkel <alehenkel17@gmail.com>
2018-03-13 15:19:54 -0600

    Add spider that succesfully gets disallowed routes

---
+++ project1/spiders/fmoore_robots.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+
+disallowedUrls = {}
+
+
+class FmooreRobotsSpider(scrapy.Spider):
+    name = 'fmoore-robots'
+    allowed_domains = ['lyle.smu.edu']
+    start_urls = ['http://lyle.smu.edu/~fmoore/robots.txt']
+
+    def parse(self, response):
+        global disallowedUrls
+        robotsText = response.css('*::text').extract_first()
+        disallowed = re.findall('Disallow: .*', robotsText)
+        for rule in disallowed:
+            route = re.match("^([^/.]+)(.*)$", rule)
+            disallowedUrls[route.group(2)] = True
+        print(disallowedUrls)
+        print(disallowedUrls['/dontgohere/'])
