9382c7b0336c4dc09a926e147db247dfd83c20c4
Alejandro Henkel <alehenkel17@gmail.com>
2018-03-13 17:33:14 -0600

    Add support for exact duplicates

--- project1/spiders/fmoore.py
+++ project1/spiders/fmoore.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+import hashlib
 from nltk import PorterStemmer
 
 index = 0
 disallowedUrls = []
 disallowedExtensions = ['.pdf', '.xlsx', '.jpg', '.gif']
+hashedFiles = []
 
 
 class FmooreSpider(scrapy.Spider):
@@ -39,16 +41,29 @@ class FmooreSpider(scrapy.Spider):
         for disallowedExt in disallowedExtensions:
             if response.url.endswith(disallowedExt):
                 return
+
         index = index + 1
         doc = "Doc{}".format(index)
         print('url @{} {}'.format(doc, response.url))
-        for text in filter(lambda x: x, map(lambda x: str(
-                x.strip()), response.css('*:not(style):not(script)::text').extract())):
+
+        # Verify exact duplicates
+        urlContent = map(lambda x: str(
+            x.strip()), response.css('*:not(style):not(script)::text').extract())
+        hashKey = hashlib.md5("".join(urlContent)).hexdigest()
+        if hashKey in hashedFiles:
+            print('{} is an exact duplicate from Doc{}'.format(
+                doc, hashedFiles.index(hashKey) + 1))
+        hashedFiles.append(hashKey)
+
+        # Filter empty words and start indexing
+        for text in filter(lambda x: x, urlContent):
             for word in text.split():
                 yield {
                     'word': PorterStemmer().stem(word),
                     'doc': doc
                 }
+
+        # Get all urls, print them and visit them
         for link in response.css('a::attr(href)').extract():
             print('link: {}'.format(str(link)))
             yield response.follow(link, callback=self.parse)
