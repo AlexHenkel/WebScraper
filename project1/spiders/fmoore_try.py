db6d6415eb69e490f6312f738ba85c939f9c69d0
Alejandro Henkel <alehenkel17@gmail.com>
2018-03-13 10:21:32 -0600

    Add second spider to succesfully parse data from one URL

---
+++ project1/spiders/fmoore_try.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class FmooreTrySpider(scrapy.Spider):
+    name = 'fmoore-try'
+    allowed_domains = ['lyle.smu.edu']
+    start_urls = ['http://lyle.smu.edu/~fmoore']
+
+    def parse(self, response):
+        for text in filter(lambda x: x, map(lambda x: str(
+                x.strip()), response.css('*::text').extract())):
+            page = response.url
+            for word in text.split():
+                yield {
+                    page: word
+                }
