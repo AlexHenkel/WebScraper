e882e59f255faffcd5d3b69b9526689020b40199
Alejandro Henkel <alehenkel17@gmail.com>
2018-05-07 14:51:48 -0500

    Implement leader-followers

--- project1/spiders/index.py
+++ project1/spiders/index.py
@@ -1,4 +1,6 @@
 import math
+from copy import deepcopy
+from random import randint
 from collections import defaultdict, Counter
 from utils import filter_word
 
@@ -15,6 +17,10 @@ class Index(object):
         self.doc_frequencies = self.create_doc_frequencies(self.docs)
         self.idf = self.create_idf(self.docs, self.doc_frequencies)
         self.index = self.create_normalized_index(self.docs)
+        # Create leader follower clusters
+        self.k_leaders = 5
+        self.leader_follower_cluster = self.create_leader_follower_cluster(
+            self.docs, self.index, len(self.docs), self.k_leaders)
         # Thesaurus used for query expansion
         self.thesaurus = {
             'beautiful': ['nice', 'fancy'],
@@ -85,7 +91,109 @@ class Index(object):
                     [i, doc_tf_idf_weights[word] / srqt_sum])
         return final_index
 
-    # Create normalized vector of query terms
+    def get_random_leaders(self, docs, docs_number, k_leaders):
+        # Randomly get k leaders indexes and their content
+        leader_indexes = []
+        for _ in range(0, k_leaders):
+            curr_rand = randint(0, docs_number - 1)
+            while curr_rand in leader_indexes:
+                curr_rand = randint(0, docs_number - 1)
+            leader_indexes.append(curr_rand)
+        return leader_indexes
+
+    def get_vector_by_doc_id(self, index, doc_id, doc_words):
+        result_vector = {}
+        for word in doc_words:
+            for curr_doc_id, curr_doc_weight in index[word]:
+                if curr_doc_id == doc_id:
+                    result_vector[word] = curr_doc_weight
+        return result_vector
+
+    def update_follwers_similarities(self, index, leader_id, leader_vector, followers_list, leader_indexes):
+        # Iterate over each term of leader's doc
+        for leader_term, leader_weight in leader_vector.items():
+            # Iterate over each document that contains current word
+            for doc_id, doc_weight in index[leader_term]:
+                # Filter other leaders
+                if doc_id in leader_indexes:
+                    continue
+                # Increase similarity between this document and the current leader
+                followers_list[doc_id][leader_id] += leader_weight * doc_weight
+        # Return modified list of follwers's similarities
+        return followers_list
+
+    def get_followers(self, leader_indexes, docs_number):
+        followers_list = []
+        for i in range(0, docs_number):
+            if not i in leader_indexes:
+                followers_list.append(i)
+        return followers_list
+
+    def add_follower_to_leader_cluster(self, follower_id, assigned_leader_id, final_clusters, leaders_followers_count, available_leaders, cluster_capacity):
+        final_clusters[assigned_leader_id].append(follower_id)
+        leaders_followers_count[assigned_leader_id] += 1
+        if leaders_followers_count[assigned_leader_id] == cluster_capacity:
+            available_leaders.remove(assigned_leader_id)
+
+    def add_unassigned_follower_to_random_leader(self, follower_id, final_clusters, leaders_followers_count, available_leaders, cluster_capacity, leader_indexes):
+        # If there are available leaders, add current follower to first available
+        if len(available_leaders):
+            self.add_follower_to_leader_cluster(
+                follower_id, available_leaders[0], final_clusters, leaders_followers_count, available_leaders, cluster_capacity)
+        # If all clusters are full (because k-leaders is not multiple of number of docs), randomly assign a leader
+        else:
+            final_clusters[leader_indexes[randint(
+                0, len(leader_indexes) - 1)]].append(follower_id)
+
+    def get_final_clusters(self, followers_list, leader_indexes, docs_number, k_leaders):
+        final_clusters = defaultdict(list)
+        pending_followers = self.get_followers(leader_indexes, docs_number)
+        # Dictionary to hold how many followers each leader has
+        leaders_followers_count = defaultdict(int)
+        # List leaders that are still accepting followers
+        available_leaders = deepcopy(leader_indexes)
+        # Cluster capacity based on document number and number of leaders
+        cluster_capacity = (docs_number - k_leaders) / k_leaders
+        # Iterate over each follower
+        for follower_id, follower_leaders in followers_list.items():
+            # Sort follower leader's in descending order
+            closest_leaders = sorted(
+                follower_leaders.items(), key=lambda x: x[1], reverse=True)
+            assigned = False
+            # Iterate over each leader to see if he still accepts follwers
+            for assigned_leader_id, _ in closest_leaders:
+                # Verify if leader still accepts followers
+                if assigned_leader_id in available_leaders:
+                    self.add_follower_to_leader_cluster(
+                        follower_id, assigned_leader_id, final_clusters, leaders_followers_count, available_leaders, cluster_capacity)
+                    assigned = True
+                    break
+            # The following applies if follower didn't find any leader
+            if not assigned:
+                self.add_unassigned_follower_to_random_leader(
+                    follower_id, final_clusters, leaders_followers_count, available_leaders, cluster_capacity, leader_indexes)
+            pending_followers.remove(follower_id)
+        # If any document has 0 similarity with all leaders, we need to assign it somewhere
+        for follower_id in pending_followers:
+            self.add_unassigned_follower_to_random_leader(
+                follower_id, final_clusters, leaders_followers_count, available_leaders, cluster_capacity, leader_indexes)
+
+        return final_clusters
+
+    def create_leader_follower_cluster(self, docs, index, docs_number, k_leaders):
+        leader_indexes = self.get_random_leaders(
+            docs, docs_number, k_leaders)
+        # This will hold the similarity between each follwer with all the leaders
+        followers_list = defaultdict(lambda: defaultdict(int))
+        for leader_id in leader_indexes:
+            curr_leader_vector = self.get_vector_by_doc_id(
+                index, leader_id, docs[leader_id])
+            followers_list = self.update_follwers_similarities(
+                index, leader_id, curr_leader_vector, followers_list, leader_indexes)
+        final_clusters = self.get_final_clusters(
+            followers_list, leader_indexes, docs_number, k_leaders)
+        return final_clusters
+
     def create_query_vector(self, query):
         final_vector = {}
         srqt_sum = float(0)
@@ -146,7 +254,6 @@ class Index(object):
             query_vector, self.index, self.titles_tokenized)
         # If few results are returned, rerun query with query expansion
         if len(results) < top_k / 2:
-            print 'init', results
             for word in query.split():
                 # Verify if lowercase query term exists in our thesaurus
                 if word.lower() in self.thesaurus:
@@ -163,8 +270,5 @@ class Index(object):
                 (curr_doc_id, curr_result, self.urls[curr_doc_id], self.titles[curr_doc_id], self.docs[curr_doc_id][:20]))
         return formatted_results
 
-    def get_doc_frequencies(self):
-        return self.doc_frequencies
-
-    def get_index(self):
-        return self.index
+    def get_clusters(self):
+        return self.leader_follower_cluster
